5460bf5c9690af5cdf6d280b0fbc56ca856fcd44
Dagm Kiros <dagimk@Dagms-MacBook-Air.local>
2026-07-04 17:40:43 -0700

    Complete Sprint 7 Python automation setup

---
+++ data.py
@@ -0,0 +1,8 @@
+URBAN_ROUTES_URL = 'https://cnt-adc4fab9-d08b-4d1f-ae57-3072fce40e4a.containerhub.tripleten-services.com'
+
+ADDRESS_FROM = 'East 2nd Street, 601'
+ADDRESS_TO = '1300 1st St'
+PHONE_NUMBER = '+1 123 123 12 12'
+CARD_NUMBER = '1234 5678 9100'
+CARD_CODE = '1111'
+MESSAGE_FOR_DRIVER = 'Stop at the juice bar, please'
